5b67d9961d934bd117258c497104ee76d5e9d319
Dhananjay_Balang <lab@practiceseries.com>
2023-08-17 12:01:03 +0530

    Successfully developed and tested create product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,17 @@
+import Product from "../models/productModel.js"
+
+//Create Product
+const createProduct= async (req,res,next)=>{
+   const product = await Product.create(req.body);
+   res.status(201).json({
+    success:true,
+    product
+   })
+}
+
+
 const getAllProducts = (req, res) => {
   res.status(200).json({ message: "Route is working fine" });
 };
 
-export default getAllProducts;
+export { getAllProducts, createProduct};
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,10 @@
 import express from "express"
 // import router  from "express"
-import getAllProducts from "../controllers/productController.js";
+import {getAllProducts, createProduct} from "../controllers/productController.js";
 
 const router = express.Router()
 
 router.route('/products').get(getAllProducts)
+router.route('/products/new').post(createProduct)
 
 export default router;
