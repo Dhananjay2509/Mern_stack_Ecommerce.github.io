1d19d1262fff5ab87314d7cc5b9a00973115a615
Dhananjay_Balang <lab@practiceseries.com>
2023-08-17 12:39:41 +0530

    Tested get all products on Postman

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,6 @@
 import Product from "../models/productModel.js"
 
-//Create Product
+//Create Product -- Admin
 const createProduct= async (req,res,next)=>{
    const product = await Product.create(req.body);
    res.status(201).json({
@@ -9,9 +9,13 @@ const createProduct= async (req,res,next)=>{
    })
 }
 
-
-const getAllProducts = (req, res) => {
-  res.status(200).json({ message: "Route is working fine" });
+//Get all products 
+const getAllProducts = async (req, res) => {
+  const product= await Product.find();
+  res.status(200).json({
+    success:true,
+    product
+   })
 };
 
 export { getAllProducts, createProduct};
