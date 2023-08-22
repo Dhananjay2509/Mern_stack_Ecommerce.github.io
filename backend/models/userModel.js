13e24016f152c1490a7112571b4bb5ed3466af91
Dhananjay_Balang <lab@practiceseries.com>
2023-08-22 14:53:34 +0530

    hashing the user password before saving so that it is not visible using bcrypt

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
-import mongoose from "mongoose"
-import validator from "validator"
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcryptjs";
 
 const userSchema= new mongoose.Schema({
    
@@ -38,6 +39,14 @@ const userSchema= new mongoose.Schema({
     resetPasswordToken:String,
     resetPasswordExpire:Date
 
+});
+
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified('password')){
+        next();
+    }
+    this.password= await bcrypt.hash(this.password,10)
 })
 
 export default mongoose.model("User",userSchema)
