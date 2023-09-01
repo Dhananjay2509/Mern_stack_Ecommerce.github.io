8b864a5ad7abb08cf4b1ff9e3d69dceb2a609bb6
Dhananjay_Balang <lab@practiceseries.com>
2023-09-01 15:51:38 +0530

    created actions, reducers and constants function for redux state management

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,45 +1,44 @@
-import {React,Fragment} from 'react'
-import { CgMouse } from 'react-icons/all'
-import "./Home.css"
-import Product from "./Product.js"
-import MetaData from '../layout/MetaData'
-
-const product ={
-    name:"Blue Tshirt",
-    images:[{url:"https://i.ibb.co/DRST11n/1.webp"}],
-    price:"₹3000",
-    _id:"product_img",
-}
+import { React, Fragment, useEffect } from "react";
+import { CgMouse } from "react-icons/all";
+import "./Home.css";
+import Product from "./Product.js";
+import MetaData from "../layout/MetaData";
+import { getProduct } from "../../actions/productAction.js";
+import { useSelector,useDispatch } from "react-redux";
 
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const {loading,error,products,productsCount}= useSelector((state)=>state.products)
+
+  useEffect(() => {
+    dispatch(getProduct())
+  }, [dispatch]);
+
   return (
     <Fragment>
-      <MetaData title="Home Page"/>
+      {loading ? "loading":<Fragment>
+      <MetaData title="Home Page" />
       <div className="banner">
         <p>Welcome to Ecommerce</p>
         <h1>FIND AMAZING PRODUCTS BELOW</h1>
 
         <a href="#container">
-            <button>
-                Scroll <CgMouse/>
-            </button>
+          <button>
+            Scroll <CgMouse />
+          </button>
         </a>
       </div>
 
       <h2 className="homeHeading">Featured Products</h2>
       <div className="container" id="container">
-        <Product product={product}/>
-        <Product product={product}/>
-        <Product product={product}/>
-        <Product product={product}/>
-        <Product product={product}/>
-        <Product product={product}/>
-        <Product product={product}/>
-        <Product product={product}/>
+        {products && products.map((product)=>
+        <Product product={product} />
+        )}
       </div>
+    </Fragment>}
     </Fragment>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
