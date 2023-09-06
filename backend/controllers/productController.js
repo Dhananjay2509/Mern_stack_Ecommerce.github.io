f45b1cb8a51d75fd9b38070139706a95758f3450
Dhananjay_Balang <lab@practiceseries.com>
2023-09-06 10:46:07 +0530

    result per page 6

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -15,7 +15,7 @@ const createProduct = catchAsyncError(async (req, res, next) => {
 
 //Get all products
 const getAllProducts = catchAsyncError(async (req, res, next) => {
-  const resultPerPage = 5;
+  const resultPerPage = 6;
   const productsCount = await Product.countDocuments();
 
   const apiFeature = new ApiFeatures(Product.find(), req.query)
