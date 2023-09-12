ef1371e5f93d8442eca333892a4a944aed6eb009
Dhananjay_Balang <lab@practiceseries.com>
2023-09-12 18:23:12 +0530

    created login and register functionality for a user and made changes to backend server and app.js

--- frontend/src/component/Product/Products.js
+++ frontend/src/component/Product/Products.js
@@ -7,12 +7,38 @@ import ProductCard from "../Home/ProductCard.js";
 import { useParams } from "react-router-dom";
 import MetaData from "../layout/MetaData.jsx";
 import Pagination from "react-js-pagination";
+import Slider from "@material-ui/core/Slider";
+import Typography from "@material-ui/core/Typography";
+import { useAlert } from "react-alert";
+import { clearErrors } from "../../actions/productAction.js";
+
+const categories = [
+  "Laptop",
+  "Footwear",
+  "Bottom",
+  "Tops",
+  "Attire",
+  "Camera",
+  "SmartPhones",
+];
 
 const Products = () => {
+
+  const alert = useAlert();
   const dispatch = useDispatch();
   const [currentPage, setCurrentPage] = useState(1);
-  const { products, loading, error, productsCount, resultPerPage } =
-    useSelector((state) => state.products);
+  const [price, setPrice] = useState([0, 25000]);
+  const [category, setCategory] = useState("");
+  const [ratings, setRatings] = useState(0);
+
+  const {
+    products,
+    loading,
+    productsCount,
+    error,
+    resultPerPage,
+    filteredProductsCount,
+  } = useSelector((state) => state.products);
 
   const { keyword } = useParams();
 
@@ -20,10 +46,19 @@ const Products = () => {
     setCurrentPage(e);
   };
 
-  useEffect(() => {
-    dispatch(getProduct(keyword, currentPage));
-  }, [dispatch, keyword, currentPage]);
+  const priceHandler = (event, newPrice) => {
+    setPrice(newPrice);
+  };
 
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+    dispatch(getProduct(keyword, currentPage, price, category, ratings));
+  }, [dispatch, keyword, currentPage, price, category, ratings, error, alert]);
+
+  let count = filteredProductsCount;
   return (
     <Fragment>
       {loading ? (
@@ -40,7 +75,45 @@ const Products = () => {
               ))}
           </div>
 
-          {resultPerPage < productsCount && (
+          <div className="filterBox">
+            <Typography>Price</Typography>
+            <Slider
+              value={price}
+              onChange={priceHandler}
+              valueLabelDisplay="auto"
+              min={0}
+              max={25000}
+            />
+
+            <Typography>Categories</Typography>
+            <ul className="categoryBox">
+              {categories.map((category) => (
+                <li
+                  className="category-link"
+                  key={category}
+                  onClick={() => setCategory(category)}
+                >
+                  {category}
+                </li>
+              ))}
+            </ul>
+
+            <fieldset>
+              <Typography component="legend">Ratings Above</Typography>
+              <Slider
+                value={ratings}
+                onChange={(e, newRating) => {
+                  setRatings(newRating);
+                }}
+                aria-labelledby="continuous-slider"
+                valueLabelDisplay="auto"
+                min={0}
+                max={5}
+              />
+            </fieldset>
+          </div>
+
+          {resultPerPage < count && (
             <div className="paginationBox">
               <Pagination
                 activePage={currentPage}
